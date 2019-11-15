b91170ab542f8a136890bcafe885d766b6757778
Ubuntu <ubuntu@ip-172-31-87-215.ec2.internal>
2019-11-15 10:04:49 +0000

    shifted static to s3

--- WearDiy/settings.py
+++ WearDiy/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'storages',
     'accounts',
     'shop',
     'cart',
@@ -128,6 +129,15 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_LOCATION="static"
+STATICFILES_STORAGE='custom_storages.StaticStorage'
+
+DEFAULT_FILE_STORAGE='custom_storages.MediaStorage'
+MEDIAFILES_LOCATION="media"
+
 
 AUTH_USER_MODEL = 'accounts.MyUser'
 
@@ -150,10 +160,20 @@ MAILGUN_SERVER_NAME = 'sandbox4f13f8deef0546cea57d7b36f2ef9946.mailgun.org'
 DEFAULT_FROM_EMAIL = "admin@sandbox4f13f8deef0546cea57d7b36f2ef9946.mailgun.org"
 FROM_EMAIL = "admin@sandbox4f13f8deef0546cea57d7b36f2ef9946.mailgun.org"
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 
 MAPBOX_ACCESS_TOKEN = os.getenv('MAPBOX_ACCESS_TOKEN')
+
+AWS_S3_OBJECT_PARAMETERS={
+    'Expires':'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl':'max-age=946800'
+}
+
+AWS_STORAGE_BUCKET_NAME="nd-django-project"
+AWS_S3_REGION_NAME="ap-southeast-1"
+AWS_ACCESS_KEY_ID=os.environ["AWS_ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY=os.environ["AWS_SECRET_ACCESS_KEY"]
+AWS_S3_CUSTOM_DOMAIN="{}.s3.amazonaws.com".format(AWS_STORAGE_BUCKET_NAME)
+
+STATICFILES_STORAGE="storages.backends.s3boto3.S3Boto3Storage"
--- payment/forms.py
+++ payment/forms.py
@@ -6,8 +6,7 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Charge
         fields = (
-            'full_name', 'phone_number', 'country', 'postcode',
-            'town_or_city', 'street_address1', 'street_address2'
+            'full_name', 'phone_number', 'country', 'postcode', 'address'
         )
 
 class PaymentForm(forms.Form):
