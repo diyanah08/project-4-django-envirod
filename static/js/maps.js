5b339907a77c17aa37065875aaa0304858b1f63a
Ubuntu <ubuntu@ip-172-31-87-215.ec2.internal>
2019-11-15 03:38:36 +0000

    changed map contents

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,84 +1,45 @@
 /* global mapboxgl */
 
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZGl5YW5haDA4IiwiYSI6ImNrMHlwam9pNzBoc2QzYnA4ZXgydXFvY2cifQ.5Ou3JPEXHCQOJ-0H4Blltw';
+let places = [{
+        'name': "Shop 1",
+        'position': [103.8937, 1.3180],
+        'image': '',
+        'description': "Details"
+    },
+    {
+        'name': "Shop 2",
+        'position': [103.8384, 1.3009],
+        'image': '',
+        'description': "Details"
+    },
+    {
+        'name': 'Shop 3',
+        'position': [103.8486, 1.3505],
+        'image': '',
+        'description': "Details"
+    }
+]
 
-let map = new mapboxgl.Map({
+mapboxgl.accessToken = "pk.eyJ1IjoiZGl5YW5haDA4IiwiYSI6ImNrMHlwam9pNzBoc2QzYnA4ZXgydXFvY2cifQ.5Ou3JPEXHCQOJ-0H4Blltw";
+
+let mapOptions = {
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
+    style: 'mapbox://styles/mapbox/streets-v11', 
     center: [103.8198, 1.3521], 
-    zoom: 11,
-});
+    zoom: 11, 
+};
+let map = new mapboxgl.Map(mapOptions);
 
-map.on('load', function() {
-    map.addLayer({
-        "id": "points",
-        "type": "symbol",
-        "source": {
-            "type": "geojson",
-            "data": {
-                "type": "FeatureCollection",
-                "features": [{
-                        "type": "Feature",
-                        "properties": {
-                            "name": "SHOP1",
-                            "icon": "marker",
-                            "color": "#EC1212",
-                            "description": "<p>DETAILS</p>"
-                        },
-                        "geometry": {
-                            "type": "Point",
-                            "coordinates": [103.8937, 1.3180]
-                        }
-                    },
-                    {
-                        "type": "Feature",
-                        "properties": {
-                            "name": "SHOP2",
-                            "icon": "marker",
-                            "color": "#EC1212",
-                            "description": "<p>DETAILS</p>"
-                        },
-                        "geometry": {
-                            "type": "Point",
-                            "coordinates": [103.8384, 1.3009]
-                        }
-                    },
-                    {
-                        "type": "Feature",
-                        "properties": {
-                            "name": "SHOP3",
-                            "icon": "marker",
-                            "color": "#EC1212",
-                            "description": "<p>DETAILS</p>"
-                        },
-                        "geometry": {
-                            "type": "Point",
-                            "coordinates": [103.8486, 1.3505]
-                        }
-                    },
-                ]
-            }
-        },
-        "layout": {
-            "icon-image": "{icon}-15",
-            "text-field": "{name}",
-            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-            "text-offset": [0, 0.6],
-            "text-anchor": "top"
-        },
-        "paint": {
-            "text-color": ["get", "color"]
-        }
+// Create a Marker
+for (let each_place of places) {
+    let m = new mapboxgl.Marker();
+    let p = new mapboxgl.Popup({
+        offset: 25
     });
-
-    map.on('click', 'points', function(e) {
-        let coordinates = e.features[0].geometry.coordinates.slice();
-        let description = e.features[0].properties.description;
-
-        new mapboxgl.Popup({ offset: [0, 1] })
-            .setLngLat(coordinates)
-            .setHTML(description)
-            .addTo(map);
-    });
-});
+    m.setLngLat(each_place.position);
+    p.setHTML(`<h1>${each_place.name}</h1>
+                <p>${each_place.description}</p>`);
+    m.setPopup(p);
+    m.addTo(map);
+}
